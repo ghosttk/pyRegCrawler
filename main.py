5ba787d95fdb15c32a0b192552667e64cfb5b9eb
ghosttk <ghosttk@hotmail.com>
2021-02-12 10:15:04 +0000

    add re.S

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import urllib.request
 import re
 
+def parseHtml(url,BodyPattern):
+     res = urllib.request.urlopen(url)
+     Content = res.read().decode('gb2312', errors='ignore')
+     #print(Content)
+     body = re.findall(BodyPattern, Content, re.S)
+     nextLink = '66'
+     return body, nextLink
 if __name__ == "__main__":
      url = 'http://www.jokeji.cn/jokehtml/mj/202101241746485.htm'
-     res = urllib.request.urlopen(url)
-     print(res.read().decode('gb2312'))
+     BodyPattern = '<span id=\"text110\">(.*)?<\/span>'
+     body, nextLink = parseHtml(url,BodyPattern)
+     print(body)
