927ce0a8ee4af9c5dfb965bb0994d393fb324d5f
ghosttk <ghosttk@hotmail.com>
2021-02-13 09:56:23 +0800

    add nextLinkPattern

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import urllib.request
 import re
 
-def parseHtml(url,BodyPattern):
+def parseHtml(url, BodyPattern, UrlPattern):
      res = urllib.request.urlopen(url)
      Content = res.read().decode('gb2312', errors='ignore')
      #print(Content)
      body = re.findall(BodyPattern, Content, re.S)
-     nextLink = '66'
+     nextLink = re.search(UrlPattern, Content, re.S).group(1)
      return body, nextLink
 if __name__ == "__main__":
      url = 'http://www.jokeji.cn/jokehtml/mj/202101241746485.htm'
      BodyPattern = '<span id=\"text110\">(.*)?<\/span>'
-     body, nextLink = parseHtml(url,BodyPattern)
-     print(body)
+     UrlPattern = '<div class=zw_page1>.*?<a href=\"(.*?)\">'
+     body, nextLink = parseHtml(url, BodyPattern, UrlPattern)
+     #print(body)
+     print(nextLink)
