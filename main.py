670410fbe125b06d702e0fac17ce28d9327a8b5f
ghosttk <ghosttk@hotmail.com>
2021-02-13 11:02:59 +0800

    find quote url bug

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
+# -*- coding: utf-8 -*-
 import urllib.request
 import re
 
 def parseHtml(url, BodyPattern, UrlPattern):
+     #url = urllib.request.quote(url)
      res = urllib.request.urlopen(url)
-     Content = res.read().decode('gb2312', errors='ignore')
+     Content = res.read().decode('gb2312', 'ignore')
      #print(Content)
      body = re.findall(BodyPattern, Content, re.S)
      nextLink = re.search(UrlPattern, Content, re.S).group(1)
+     if nextLink != '':
+         nextLink = re.sub(r'/\d*?.htm$', '/'+nextLink, url)
      return body, nextLink
 if __name__ == "__main__":
      url = 'http://www.jokeji.cn/jokehtml/mj/202101241746485.htm'
      BodyPattern = '<span id=\"text110\">(.*)?<\/span>'
      UrlPattern = '<div class=zw_page1>.*?<a href=\"(.*?)\">'
      body, nextLink = parseHtml(url, BodyPattern, UrlPattern)
-     #print(body)
-     print(nextLink)
+     while nextLink !='':
+         print(nextLink)
+         body, nextLink = parseHtml(nextLink, BodyPattern, UrlPattern)
+         #print(body)
+
