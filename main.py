2a707e6e05955ce443e2c63372fd785651a17d5f
ghosttk <ghosttk@hotmail.com>
2021-02-15 10:43:17 +0800

    add __del__()

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ class RegParser():
     def __init__(self, baseUrl, count):
         self.baseUrl = baseUrl
         self.count =count
+        self.strToWrite = []
+        self.fo = open('strbodys.txt', 'a+')
         self.cp = configparser.ConfigParser()
         curr_dir = os.path.dirname(os.path.realpath(__file__))
         config_file = curr_dir + os.sep + "params.conf"
@@ -18,11 +20,16 @@ class RegParser():
         self.BodyPattern = self.cp.get(self.baseUrl, 'BodyPattern')
         self.UrlPattern = self.cp.get(self.baseUrl, 'UrlPattern')
         self.trunkPattern = self.cp.get(self.baseUrl, 'trunkPattern')
+    def __del__(self):
+         self.cp.set(self.baseUrl, 'url', self.nextLink)
+         self.fo.writelines(self.strToWrite)
+         self.fo.close()
+         print('deleted')
     def parseBaseUrl(self, url):
         baseUrl = re.match('https?://.*?/', url).group()
         return baseUrl
     def fmtBodyString(self, strBody):
-        result = re.sub('<.*>', '', strBody)
+        result = re.sub('<.*?>', '', strBody)
         return result
     def parseHtml(self):
         try:
@@ -31,18 +38,25 @@ class RegParser():
             print(e)
         Content = res.read().decode('gb2312', 'ignore')
         body = re.findall(self.BodyPattern, Content, re.S)[0]
-        body = self.fmtBodyString(body)
+        #body = self.fmtBodyString(body)
         #body = re.findall('[\u4e00-\u9fa5]+', body[0])
         nextLink = re.search(self.UrlPattern, Content, re.S).group(1)
         if nextLink != '':
             nextLink = re.sub('\.\./\.\./', '',  nextLink)
             nextLink = self.baseUrl + nextLink
+        self.nextLink = nextLink
+        print(nextLink)
         return body, nextLink
 if __name__ == "__main__":
      baseUrl = 'http://www.jokeji.cn/'
      myRegParser = RegParser(baseUrl, 3)
-     bodyString, nextUrl = myRegParser.parseHtml()
-     print(bodyString)
+     bodyString, nextLink = myRegParser.parseHtml()
+     myRegParser.strToWrite.append(bodyString)
+     while(nextLink!=''):
+         myRegParser.url = urllib.parse.quote(nextLink, safe=string.printable)
+         bodyString, nextLink = myRegParser.parseHtml()
+         myRegParser.strToWrite.append(bodyString)
+
      '''
      BodyPattern = '<span id=\"text110\">(.*)?<\/span>'
      Pattern = '<div class=zw_page1>.*?<a href=\"(.*?)\">'
